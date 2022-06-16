e2b2e74fd72b971f3f8c222e1b7c0fba3c4bcb4f
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2022-06-16 07:55:53 +0700

    fix : minor compile error

--- Stack.c
+++ Stack.c
@@ -1,24 +1,9 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "Stack.h"
 
 #define False 0;
 #define True 1;
-
-Stack *createStack(int size)
-{
-    Stack *stack;
-
-    stack = malloc(sizeof(Stack));
-    stack->size = size;
-    stack->data = malloc(sizeof(int) * size);
-    stack->pointer = 0;
-    stack->push = &push;
-    stack->pop = &pop;
-    stack->print = &print;
-    stack->isEmpty = &isEmpty;
-
-    return stack;
-}
 
 /**
  * @brief Push the value into stack
@@ -28,7 +13,7 @@ Stack *createStack(int size)
  */
 void push(Stack *stack, int value)
 {
-    if (stack->pointer >= stack->size || stack->pointer <= 0)
+    if (stack->pointer >= stack->size)
         exit(0);
 
     stack->data[stack->pointer] = value;
@@ -43,7 +28,7 @@ void push(Stack *stack, int value)
  */
 int pop(Stack *stack)
 {
-    if (stack->pointer >= stack->size || stack->pointer <= 0)
+    if (stack->pointer >= stack->size)
         exit(0);
 
     stack->pointer--;
@@ -64,7 +49,10 @@ void print(Stack *stack)
 {
     for (int i = 0; i < stack->pointer; i++)
     {
-        printf("%d, ", stack->data[i]);
+        if (i >= stack->pointer - 1)
+            printf("%d", stack->data[i]);
+        else
+            printf("%d, ", stack->data[i]);
     }
     printf("\n");
 }
@@ -80,4 +68,19 @@ int isEmpty(Stack *stack)
     if (stack->pointer <= 0)
         return True;
     return False;
+}
+
+Stack *createStack(int size)
+{
+    Stack *stack;
+    stack = malloc(sizeof(Stack));
+    stack->size = size;
+    stack->data = malloc(sizeof(int) * size);
+    stack->pointer = 0;
+    stack->push = &push;
+    stack->pop = &pop;
+    stack->print = &print;
+    stack->isEmpty = &isEmpty;
+
+    return stack;
 }
